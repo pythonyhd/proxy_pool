ea7a2736657a74b192fcfa6006eaf789636bea8e
admin_huadong <18353626676@163.com>
2019-12-10 19:58:54 +0800

    重新定义通用下载器，retry模块容易出问题

--- proxypool/utils.py
+++ proxypool/utils.py
@@ -6,11 +6,49 @@ from retrying import retry
 import requests
 from requests.exceptions import ConnectionError, ConnectTimeout
 from fake_useragent import UserAgent
+
 header = {"User-Agent": UserAgent().random}
 
 
+def downloader(url, method, data=None, headers={}, proxies=None, retry_times=10):
+    """
+    通用下载器
+    :param url: url
+    :param method: 请求方法，只支持get跟post
+    :param data: post表单参数
+    :param proxies: 代理
+    :param retry_times: 重试次数
+    :return: 响应结果
+    """
+    headers = dict(header, **headers)
+    while retry_times > 0:
+        try:
+            if method == 'GET':
+                if proxies:
+                    res = requests.get(url=url, headers=headers, proxies=proxies, timeout=20)
+                else:
+                    res = requests.get(url=url, headers=headers, timeout=20)
+            else:
+                if proxies:
+                    res = requests.post(url=url, data=data, headers=headers, proxies=proxies, timeout=20)
+                else:
+                    res = requests.post(url=url, data=data, headers=headers, timeout=20)
+            if res.status_code in [200, 201, 202]:
+                return res.text
+        except ConnectionError:
+            print("抓取失败", url)
+            return None
+        except (ConnectTimeout, TimeoutError):
+            return None
+        except Exception as e:
+            print(f'请求出错:{repr(e)}--需要重试')
+            if retry_times > 0:
+                retry_times -= 1
+            print(f'达到重试次数，仍然抓取失败')
+
+
 @retry(stop_max_attempt_number=8)
-def downloader(url, method, data=None, options={}):
+def downloader_old(url, method, data=None, options={}):
     """
     通用下载器，只处理get跟post请求
     :param url:
